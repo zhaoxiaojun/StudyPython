5cd97ff39966e38fddac8cf0b8d043468b2fb8be
Defias <yechaohui@tuandai.com>
2016-04-13 11:52:05 +0800

    update

--- TestAutoWeb/test_lettuce/comp_factorial/comp_factorial.py
+++
@@ -1,8 +0,0 @@
-#coding=utf-8
-
-def comp_factorial(num):
-    c = 1
-    for i in range(num):
-        i = i+1
-        c = c*i
-    return c
--- TestAutoWeb/test_lettuce_webdriver/features/support/env.py
+++
@@ -1,9 +0,0 @@
-#coding=utf-8
-from selenium import webdriver
-from lettuce import before, world
-import lettuce_webdriver.webdriver
-
-@before.all
-def setup_browser():
-    world.browser = webdriver.Firefox()
-    
